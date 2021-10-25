0d8cff26c967346269db30150540c8d99e3cedad
herveGuigoz <herve.guigoz@gmail.com>
2021-10-26 01:03:50 +0200

    🔥 refactor: http

---
+++ client/dashlist/lib/src/services/http/http_headers.dart
@@ -0,0 +1,20 @@
+abstract class Header {
+  const Header(this.key, this.value);
+
+  static Map<String, String> merge(List<Header> values) {
+    return {for (var v in values) v.key: v.value};
+  }
+
+  final String key;
+  final String value;
+
+  Map<String, String> toMap() => {key: value};
+}
+
+class JsonContentType extends Header {
+  const JsonContentType() : super('Content-Type', 'application/json');
+}
+
+class AcceptJson extends Header {
+  const AcceptJson() : super('Accept', 'application/json');
+}
